302dcee7ce00a72859e1a940df2bb12a7ce2f580
sui958337821 <suidongbo@baidu.com>
2022-10-29 19:08:11 +0800

    leetcode

--- code/leetcode/interleaving-string/solution2.py
+++ code/leetcode/interleaving-string/solution2.py
@@ -1,20 +1,24 @@
+# DFS
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
             return False
 
-        def dfs(i, j):
+        def dps(i, j):
             if i == len(s1) and j == len(s2):
                 return True
-            choose_s1, choose_s2 = False, False
-            if i < len(s1) and s1[i] == s3[i + j]:
-                choose_s1 = dfs(i + 1, j)
-            if j < len(s2) and s2[j] == s3[i + j]:
-                choose_s2 = dfs(i, j + 1)
 
-            return choose_s1 or choose_s2
+            choose_1 = False
+            choose_2 = False
 
-        return dfs(0, 0)
+            if i < len(s1) and s1[i] == s3[i+j]:
+                choose_1 = dps(i + 1, j)
+            if j < len(s2) and s2[j] == s3[i+j]:
+                choose_2 = dps(i, j + 1)
+
+            return choose_1 or choose_2
+
+        return dps(0, 0)
 
 a = Solution()
 print(a.isInterleave("aabcc", "dbbca", "aadbbcbcac"))
